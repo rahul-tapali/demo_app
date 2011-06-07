66e4f89ca98e558ca16dbc1a5c1ee08b5aa60a0a
rahul-tapali <rahul.tapali100@gmail.com>
2011-06-07 15:52:25 +0530

    Change 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+attr_accessible :email
+
+email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 validates :email, :presence   => true,
                   :format     => { :with => email_regex },
                   :uniqueness => true
