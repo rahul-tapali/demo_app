6b518c95329def98f220a318de78af36d2ae4bb0
rahul-tapali <rahul.tapali100@gmail.com>
2011-06-07 16:03:03 +0530

    Final validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ attr_accessible :email
 
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+validates :email, :length     => { :minimum => 1 } 
+
 validates :email, :presence   => true,
                   :format     => { :with => email_regex },
                   :uniqueness => true
