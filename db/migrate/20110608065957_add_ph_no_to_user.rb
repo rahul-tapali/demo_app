14faf4fadfd5b041e208edab4504b31b86d4cbff
rahul-tapali <rahul.tapali100@gmail.com>
2011-06-08 12:37:15 +0530

    Update9

--- db/migrate/20110608065957_add_ph_no_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddPhNoToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :dept, :string
-  end
-
-  def self.down
-    remove_column :users, :dept
-  end
-end
--- db/migrate/20110608070120_remove_ph_no_from_user.rb
+++
@@ -1,9 +0,0 @@
-class RemovePhNoFromUser < ActiveRecord::Migration
-  def self.up
-    remove_column :users, :dept
-  end
-
-  def self.down
-    add_column :users, :dept, :string
-  end
-end
