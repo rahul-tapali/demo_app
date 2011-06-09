b95575ae0d0fbca41c66895e6ce11cb45aa3cce0
rahul-tapali <rahul.tapali100@gmail.com>
2011-06-09 10:00:31 +0530

    Update45

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 ph_no_regex = /(\d{10}|\+91\d{10})/ 
 validates :email, :length     => { :minimum => 1 } 
 
-validates :ph_no, :length     => { :is => 10 },
-                  :format     => { :with => ph_no_regex }
+validates :ph_no, :format     => { :with => ph_no_regex }
                   
 validates :email, :presence   => true,
                   :format     => { :with => email_regex },
